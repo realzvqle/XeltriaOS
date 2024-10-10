d7270c73be9fd2efa32aa0f0e82621cc8ebbedd9
realzvqle <realzvqle@gmail.com>
2024-10-10 16:28:52 -0500

    happy happy happppyyyy =3

--- kernel/src/kmain.c
+++ kernel/src/kmain.c
@@ -1,10 +1,12 @@
 #include "uart.h"
 #include "malloc.h"
 
-
+extern void test();
 
 
 void KiEntry(void) {
+    test();
+    KiSerialPrint("Hi!!");
     char* idk = KiSerialGets(1024);
     KiSerialPrint(idk);
     KiFreeMemory(idk);
